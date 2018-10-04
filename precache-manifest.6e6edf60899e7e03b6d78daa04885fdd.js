e67f8ec28b59c5740537a2d248966c725405f8fe
Raoudha Nouisser <rawdhanouisser@gmail.com>
2018-10-04 13:21:07 -0700

    Updates

---
+++ precache-manifest.6e6edf60899e7e03b6d78daa04885fdd.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "e96da754f145f1848e6ded634bac2f5e",
+    "url": "/reactweb/static/media/img12.e96da754.jpg"
+  },
+  {
+    "revision": "6b5d5bdc67cdf46abd62",
+    "url": "/reactweb/static/css/main.2983521c.chunk.css"
+  },
+  {
+    "revision": "381fa64f0ae690efe811",
+    "url": "/reactweb/static/js/1.381fa64f.chunk.js"
+  },
+  {
+    "revision": "02266de139efa31b110e",
+    "url": "/reactweb/static/js/runtime~main.02266de1.js"
+  },
+  {
+    "revision": "15e754290797555440b4b30f60767ab6",
+    "url": "/reactweb/static/media/logoApp.15e75429.png"
+  },
+  {
+    "revision": "2ffb1e2790f9bb61a560676999b60edf",
+    "url": "/reactweb/static/media/welcome.2ffb1e27.jpg"
+  },
+  {
+    "revision": "589a8265870ed10193a9f450257ca023",
+    "url": "/reactweb/static/media/img11.589a8265.jpg"
+  },
+  {
+    "revision": "6b5d5bdc67cdf46abd62",
+    "url": "/reactweb/static/js/main.6b5d5bdc.chunk.js"
+  },
+  {
+    "revision": "0f1511c1e7123604b71a0b79904e9cd9",
+    "url": "/reactweb/static/media/img13.0f1511c1.jpg"
+  },
+  {
+    "revision": "e4c06a331ea41bedd336ad3ad5bb0d24",
+    "url": "/reactweb/static/media/img14.e4c06a33.jpg"
+  },
+  {
+    "revision": "4493dab542a6e0ca5648e677b1d77d8a",
+    "url": "/reactweb/static/media/img15.4493dab5.jpg"
+  },
+  {
+    "revision": "5a3d3e5d278bf799165469853b7c910f",
+    "url": "/reactweb/static/media/img16.5a3d3e5d.jpg"
+  },
+  {
+    "revision": "51643b9f4486f64ba169de071a9b9200",
+    "url": "/reactweb/static/media/backgroundApp.51643b9f.jpg"
+  },
+  {
+    "revision": "12be1ff6ed6b5cf981a12f7b5bc1e37f",
+    "url": "/reactweb/index.html"
+  }
+];
